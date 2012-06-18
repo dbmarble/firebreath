55ae35938b7ddf17e370e31f71d07ac59a99f50b
David Marble <davidbmarble@gmail.com>
2012-06-18 09:01:36 -0700

    base project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firebreath::Application.config.secret_token = '6d44c48c350370163285c397871dee4a6b0540378a198cc9093935015f929549a1765a15d258508259f7090e28a3678d9673353e63ed3c5e2d3144f6c3b91163'
